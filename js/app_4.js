2b855c334c90d3b5eef6f5c92a0a96a7c168431c
Will Breitkreutz <will.l.breitkreutz@gmail.com>
2014-11-13 15:10:02 -0600

    Update app_4.js

--- js/app_4.js
+++ js/app_4.js
@@ -14,7 +14,7 @@ map.setView([39, -96], 4);
 ///////////////////////////////////////////////////////////////////////////
 // This is the area we're going to use to add data to our map
 
-var dataFileToAdd = 'data/powercat.geojson'; //<- Point this to the file that you want to include on the map
+var dataFileToAdd = 'data/bike_routes.geojson'; //<- Point this to the file that you want to include on the map
 var dataToAdd;
 
 var featureLayer = L.mapbox.featureLayer()
@@ -59,7 +59,7 @@ var clickHandler = function(e){
   $('#info').fadeIn(400,function(){
     var info = '';
 
-    info = '<div>You clicked on the layer with the id of ' + feature.properties.id + '</div>';
+    info = '<div>Sweet bike route named ' + feature.properties.name + '</div>';
 
     $('#info').append(info);
   });
