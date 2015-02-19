40cf661f7a0aa38f7d663658b5069229d978cd86
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2015-02-19 13:52:12 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
+var mapId = 'will-breitkreutz.l8mm447l'; //<- this references the ugly green map that I made
 var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
