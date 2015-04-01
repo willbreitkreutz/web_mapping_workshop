ad5833f5ff54078435639c9cd1e7a0b3b0d16498
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2015-03-31 19:33:58 -0500

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -10,3 +10,17 @@ var map = L.mapbox.map('map', mapId);
 
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
+
+var dataFileToAdd = 'data/parks.geojson';
+
+var featureLayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map);
+
+featureLayer.on('ready',function(){
+  this.setStyle({
+    'color':'#ec008c',
+    'fillColor':'#ec008c',
+    'weight':4,
+    'opacity':.7
+  });
+  map.fitBounds(featureLayer.getBounds());
+});
