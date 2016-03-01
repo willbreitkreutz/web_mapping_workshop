a2e9d89e21b541dbd76ba59a1dceb90e6b1e2e55
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-02-29 19:54:52 -0600

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -1,1 +1,18 @@
-// some
+// Here is the javascript setup for a basic map:
+
+// Enter your mapbox map id here to reference it for the base layer,
+// this one references the ugly green map that I made.
+var mapId = 'will-breitkreutz.k6fj4l3f';
+
+// And this is my token, use yours.
+var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+
+// Create the map object with your mapId and token, 
+// referencing the DOM element where you want the map to go.
+L.mapbox.accessToken = token;
+var map = L.mapbox.map('map', mapId);
+
+// Set the initial view of the map to the whole US
+map.setView([39, -96], 4);
+
+// Great, now we have a basic web map!
