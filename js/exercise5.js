b1bddad88dfa5f7af1e37d08c761236e8ce47480
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-03-10 16:13:27 -0500

    Created exercise5.js

---
+++ js/exercise5.js
@@ -0,0 +1,16 @@
+
+var dataFileToAdd = 'data/parks.geojson';
+
+var featureLayer = L.mapbox.featureLayer()
+    .loadURL(dataFileToAdd)
+    .addTo(map);
+
+featureLayer.on('ready', function() {
+    this.setStyle({
+        "color": "#6583BF",
+        "fillColor": "#6583BF",
+        "weight": .5,
+        "opacity": 0.65
+    });
+    map.fitBounds(featureLayer.getBounds());
+});
