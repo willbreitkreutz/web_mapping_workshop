0eedf660d6214d6cfa73cc4348962712217ccca3
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-03-10 17:01:40 -0500

    Updated exercise_7.js

--- js/exercise_7.js
+++ js/exercise_7.js
@@ -1,16 +1,7 @@
 // exercise 7
-//
-// Clear out the info panel when you click somewhere in the map
-map.on('click',function(e){
-	$('#info').fadeOut(200);
-    $('#info').empty();
-});
-
-// Use this function to handle the click event on the data
 var clickHandler = function(e){
   $('#info').empty();
 
-  //e is the click event in the browser, it's target is our element that was clicked
   var feature = e.target.feature;
 
   $('#info').fadeIn(400,function(){
@@ -25,9 +16,13 @@ var clickHandler = function(e){
   });
 };
 
-// Register the click event on each of the features in the map
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
     layer.on('click', clickHandler);
   });
+});
+
+map.on('click',function(e){
+	$('#info').fadeOut(200);
+    $('#info').empty();
 });
