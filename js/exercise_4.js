b88f700fa155447842ca54386bfff58da747f9a0
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-04-25 10:43:23 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -33,9 +33,34 @@ featureLayer.on('ready', function(){
   map.fitBounds(featureLayer.getBounds());
 })
 
+var clickHandler = function(e){
+
+  $('#info').empty();
+  
+  var feature = e.target.feature;
+  
+  $('#info').fadeIn(400, function(){
+  
+  	var info = '';
+    
+    info += '<div>',
+    info += 	'<h2>' + feature.properties.LABEL + '</h2>';
+    info += 	'<p>' + feature.properties.LOCATION + '</p>';
+    info += '</div>';
+    
+    $('#info').append(info);
+    
+  })
+  
+}
+
 featureLayer.on('ready', function(){
 	this.eachLayer(function(layer){
-    	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
+    	layer.on('click', clickHandler);
     })
 })
 
+map.on('click', function(){
+	$('#info').fadeOut(200);
+  	$('#info').empty();
+})
