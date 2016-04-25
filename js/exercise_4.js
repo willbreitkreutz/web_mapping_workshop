d68f14dae134ba4520a646ecc4d80844903e1e66
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-04-25 09:36:54 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -16,3 +16,19 @@ var map = L.mapbox.map('map', mapId);
 map.setView([39, -96], 4);
 
 // Great, now we have a basic web map!
+
+var dataFileToAdd = 'data/parks.geojson';
+
+var featureLayer = L.mapbox.featureLayer();
+	featureLayer.loadURL(dataFileToAdd);
+	featureLayer.addTo(map);
+
+featureLayer.on('ready', function(){
+  this.setStyle({
+  	"color": "#6583BF",
+    "fillColor": "#6583BF",
+    "weight": .5,
+    "opacity": 0.65
+  })
+  map.fitBounds(featureLayer.getBounds());
+})
