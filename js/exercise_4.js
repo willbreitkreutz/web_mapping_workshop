41751e978112379697d447c5df89bd28bc7fd537
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-05-02 16:25:55 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -74,4 +74,23 @@ map.on('click', function(){
 	$('#sidebar').fadeOut(200);
 })
 
+var myLocation = L.mapbox.featureLayer().addTo(map);
+
+map.on('locationfound', function(e){
+	myLocation.setGeoJSON({
+    	type: 'Feature',
+      	geometry: {
+        	type: 'Point',
+          	coordinates: [ e.latlng.lng, e.latlng.lat ]
+        },
+      	properties: {
+        	"title": "Here I am!",
+          	"marker-color": "#ff8888",
+          	"marker-symbol": "star"
+        }
+    })
+})
+
+map.locate({setView: true})
+
 
