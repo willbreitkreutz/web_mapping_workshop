e3884845fa86d8a0bcb2bdca0a4f9752df02ea9b
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-05-02 15:22:24 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -34,6 +34,12 @@ featureLayer.on('ready', function(){
     map.fitBounds(featureLayer.getBounds());
 })
 
+featureLayer.on('ready', function(){
+	this.eachLayer(function(layer){
+    	layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+    })
+})
+
 
 
 
