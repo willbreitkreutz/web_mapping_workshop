ce97e475a851b0c33741f879464f60d575adbb18
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-05-02 17:37:03 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -128,6 +128,14 @@ function getDirections(frm, to){
               	"stroke-width": 8
             }
         })
+        
+        $('#directions').fadeIn(400, function(){
+          	var summary = data.trip.summary
+        	$('#summary').empty();
+          	$('#distance').text((Math.round(summary.length * 100) / 100) + data.trip.units);
+          	$('#time').text((Math.round(summary.time / 60 * 100) / 100) + ' min');
+        })
+        
     })
 }
 
