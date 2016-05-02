b6a326b4e2f748e9c7d9c8292b05cedd50684c80
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-05-02 17:53:23 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -137,7 +137,20 @@ function getDirections(frm, to){
           	$('#distance').text((Math.round(summary.length * 100) / 100) + ' ' + data.trip.units);
           	$('#time').text((Math.round(summary.time / 60 * 100) / 100) + ' min');
           
-          
+          data.trip.legs[0].maneuvers.forEach(function(item){
+            
+            var direction = '';
+            direction += '<li class="instruction" data-begin=' + item.begin_shape_index + ' data-end=' + item.end_shape_index + '>';
+            if(item.verbal_post_transition_instruction){
+            	direction += '<p class="post-transition">' + item.verbal_post_transition_instruction + '</p>'
+            }
+            if(item.verbal_pre_transition_instruction){
+            	direction += '<p class="post-transition">' + item.verbal_pre_transition_instruction + '</p>'
+            }
+            direction += '</li>';
+           	$('#summary').append(direction);
+            
+          })
           
         })
         
