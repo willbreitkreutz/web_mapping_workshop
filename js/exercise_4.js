c8105902fe213ecf215ca9cd0e047c405e3e41c9
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-05-02 17:02:39 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -62,6 +62,10 @@ var clickHandler = function(e){
       	info += '</div>';
       	$('#info').append(info);
     })
+    
+    var myGeoJSON = myLocation.getGeoJSON();
+    
+    getDirections(myGeoJSON.geometry.coordinates, feature.geometry.coordinates);
 }
 
 featureLayer.on('ready', function(){
@@ -92,5 +96,39 @@ map.on('locationfound', function(e){
 })
 
 map.locate({setView: true})
+
+var routeLine = L.mapbox.featureLayer().addTo(map);
+
+function getDirections(frm, to){
+	var jsonPayload = JSON.stringify({
+    	locations: [
+          {lat: frm[1], lon: frm[0]},
+          {lat: to[1], lon: to[0]}
+        ],
+      	costing: 'pedestrian',
+      	units: 'miles'
+    })
+    $.ajax({
+    	url: 'http://valhalla.mapzen.com/route',
+      	data: {
+        	json: jsonPayload,
+          	api_key: 'valhalla-gwtf3x2'
+        }
+    }).done(function(data){
+    	var routeShape = polyline.decode(data.trip.legs[0].shape);
+      	routeLine.setGeoJSON({
+        	type: 'Feature',
+          	geometry: {
+            	type: 'LineString',
+              	coordinates: routeShape
+            },
+          	properties: {
+            	"stroke": "#ed23f1",
+              	"stroke-opacity": 0.8,
+              	"stroke-width": 8
+            }
+        })
+    })
+}
 
 
