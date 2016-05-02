cf7c7bd2749312a2b2440689076686cc11b2915b
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2016-05-02 15:45:51 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -34,12 +34,41 @@ featureLayer.on('ready', function(){
     map.fitBounds(featureLayer.getBounds());
 })
 
+//featureLayer.on('ready', function(){
+//	this.eachLayer(function(layer){
+//    	layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+//    })
+//})
+
+var clickHandler = function(e){
+	$('#info').empty();
+  
+  	var feature = e.target.feature;
+  
+  	$('#sidebar').fadeIn(400, function(){
+    	var info = '';
+      
+      	info += '<div>';
+      	info += '<h2>' + feature.properties.name + '</h2>'
+        if(feature.properties.cuisine){
+          info += '<p>' + feature.properties.cuisine + '</p>';
+        }
+        if(feature.properties.phone){
+          info += '<p>' + feature.properties.phone + '</p>';
+        }
+      	if(feature.properties.website){
+          info += '<p><a href="' + feature.properties.website + '">' + feature.properties.website + '</a></p>';
+        }
+      	info += '</div>';
+      	$('#info').append(info);
+    })
+}
+
 featureLayer.on('ready', function(){
 	this.eachLayer(function(layer){
-    	layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+    	layer.on('click', clickHandler);
     })
 })
-
 
 
 
